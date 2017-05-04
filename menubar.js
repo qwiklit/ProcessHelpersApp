b85aa69855f79eedde0da034f37f24512ce2ffcf
Tapestry Labs <qwiklit@gmail.com>
2017-05-04 12:51:49 -0400

    First push

---
+++ menubar.js
@@ -0,0 +1,38 @@
+document.getElementById("sidebar-wrapper").innerHTML =
+    
+'<ul class="sidebar-nav">' +
+'<li class="sidebar-brand">' +
+'<a href="#">' +
+'<img src="icons/magnifying-glass34.png">Search</a>' +
+'</li>' +
+'<li>' +
+'<a href="#">My Profile</a>' +
+'</li>' +
+'<li>' +
+'<a href="#">Courthouses</a>' +
+'</li>' +
+'<li>' +
+'<a href="#">Members</a>' +
+'</li>' +
+'<li>' +
+'<a href="#">Request a service</a>' +
+'</li>' +
+'<li>' +
+'<a href="#">Posted Requests</a>' +
+'</li>' +
+'<li>' +
+'<a href="#">Social Room</a>' +
+'</li>' +
+'<li>' +
+'<a href="#">Messages</a>' +
+'</li>' +
+'<li>' +
+'<a href="#">Newsroom</a>' +
+'</li>' +
+'<li>' +
+'<a href="#">Terms of Service</a>' +
+'</li>' +
+'<li>' +
+'<a href="#">Settings</a>' +
+'</li>' +
+'</ul>' ;
